2136685948215f17d7b41e08dea3eedfa32b48d4
JesusAngelMM <jesusangelmartinezmendoza0702@gmail.com>
2024-01-24 20:38:04 -0600

    18.3 Declaración de una Clase en C++

---
+++ Clases y Objetos/Punto.cpp
@@ -0,0 +1,21 @@
+#include "Punto.h"
+
+//Establecer el valor de X
+void Punto::setX(int valorX){
+    x = valorX;
+}
+
+//Establecer el valor de Y
+void Punto::setY(int valorY){
+    y = valorY;
+}
+
+//Obtener el valor de X
+int Punto::getX(){
+    return x;
+}
+
+//Obtener el valor de Y
+int Punto::getY(){
+    return y;
+}
