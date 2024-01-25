aef5cb9ceccfc433873e22953db2ca3d172e8d35
JesusAngelMM <jesusangelmartinezmendoza0702@gmail.com>
2024-01-25 16:13:32 -0600

    18.4 Creación de Objetos en C++

--- Clases y Objetos/main.cpp
+++ Clases y Objetos/main.cpp
@@ -6,8 +6,16 @@ using namespace std;
 int main(int argc, char** argv){
     Punto p1(2,1);  //Creación de un objeto estático
     
-    cout<<"El valor de X es ahora: "<<p1.getX()<<endl;
-    cout<<"El valor de Y es ahora: "<<p1.getY()<<endl;
+    cout<<"El valor de X es: "<<p1.getX()<<endl;
+    cout<<"El valor de Y es: "<<p1.getY()<<endl;
+
+    Punto* p2 = new Punto(5,3);
+
+    p2->setX(10);
+    p2->setY(6);
+
+    cout<<"El valor de X es: "<<p2->getX()<<endl;
+    cout<<"El valor de Y es: "<<p2->getY()<<endl;
 
     return 0;
 }
