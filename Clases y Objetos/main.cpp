31587cc67cf47562bf3d5b898b280853a92c4976
JesusAngelMM <jesusangelmartinezmendoza0702@gmail.com>
2024-08-13 17:47:48 -0600

    18.5. Visibilidad de los miembros de la clase (Encapsulamiento)

--- Clases y Objetos/main.cpp
+++ Clases y Objetos/main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char** argv){
     cout<<"El valor de X es: "<<p1.getX()<<endl;
     cout<<"El valor de Y es: "<<p1.getY()<<endl;
 
-    Punto* p2 = new Punto(5,3);
+    Punto* p2 = new Punto(5,3);  //Creación de un objeto dinámico
 
     p2->setX(10);
     p2->setY(6);
@@ -19,3 +19,8 @@ int main(int argc, char** argv){
 
     return 0;
 }
+
+/*
+Ejecución:
+cd "d:\Documentos\Programacion\Cpp\ats\18_POO_ClasesYObjetos\Clases y Objetos\" ; if ($?) { g++ punto.cpp main.cpp -o main } ; if ($?) { .\main }
+*/
